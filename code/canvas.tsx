6c6fcb576fadcbf6b5b8f5b59f72f0bbba6cdb47
Mark Warness <markwarness@gmail.com>
2019-10-29 19:34:17 -0400

    initial commit

---
+++ code/canvas.tsx
@@ -0,0 +1,5 @@
+// WARNING: this file is auto generated, any changes will be lost
+import { createDesignComponent, CanvasStore } from "framer"
+const canvas = CanvasStore.shared(); // CANVAS_DATA;
+export const Layout_Funk = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_bw8Qe3GkH", {}, 272,200);
+export const Layout_Funktwo = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_QI_d1zD8g", {}, 272,200);
